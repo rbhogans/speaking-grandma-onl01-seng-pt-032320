19372c72bf1472010d07084052434564e03daed7
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-26 05:28:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == " I LOVE YOU TOO GRANDMA! "
+  if phrase == " I LOVE YOU GRANDMA! "
     return "I LOVE YOU TOO PUMPKIN! "
   elsif phrase.upcase!= phrase
     return "HUH?! SPEAK UP, SONNY!"
